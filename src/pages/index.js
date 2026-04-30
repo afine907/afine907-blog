bdeee75a76d05d70e93440857c85baf2e309b666
afine907 <1049905106@qq.com>
2026-04-30 14:43:40 +0800

    fix: browse button 404 and sidebar width in Chrome
    - Change '浏览文章' button link from /docs/intro to / (routeBasePath is '/')
    - Fix sidebar scrollbar-gutter: stable causing cut-off in Chrome
    - Add explicit sidebar viewport overflow-y: auto

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ function HomepageHeader() {
             <div className={styles.buttons}>
               <Link
                 className="button button--secondary button--lg"
-                to="/docs/intro">
+                to="/">
                 浏览文章 ↓
               </Link>
               <Link
